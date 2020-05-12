c47b1f9d712b39a8231fa6a67162e257aa7b9c90
gyanendraknojiya <gyanendrak064@gmail.com>
2020-05-12 23:44:28 +0530

    changes

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
-function playbtn(source) {
+function playbtn(source, key) {
     var audio = new Audio("./sounds/" + source + ".mp3");
+    var pressedkey = document.querySelector("." + key);
+    pressedkey.classList.add("pressed");
+    setTimeout(function() {
+        pressedkey.classList.remove("pressed")
+    }, 100);
     return audio.play();
 }
 document.addEventListener('keydown', function(event) {
     if (event.keyCode === 87) {
-        return playbtn("tom-1");
+        return playbtn("tom-1", "w");
     } else if (event.keyCode === 65) {
-        return playbtn("tom-2");
+        return playbtn("tom-2", "a");
     } else if (event.keyCode === 83) {
-        return playbtn("tom-3");
+        return playbtn("tom-3", "s");
     } else if (event.keyCode === 68) {
-        return playbtn("tom-4");
+        return playbtn("tom-4", "d");
     } else if (event.keyCode === 74) {
-        return playbtn("crash");
+        return playbtn("crash", "j");
     } else if (event.keyCode === 75) {
-        return playbtn("kick-bass");
+        return playbtn("kick-bass", "k");
     } else if (event.keyCode === 76) {
-        return playbtn("snare");
+        return playbtn("snare", "l");
     }
 });
-
-// var array = document.querySelectorAll("button");
-// for (let index = 0; index < array.length; index++) {
-//     array[index].addEventListener("click", checkclick);
-// }
-
-// function checkclick() {
-//     this.style.color = "white";
-//     var audio = new Audio("./sounds/tom-1.mp3");
-//     audio.play();
-
-// }
